27d68af524b30af2393ca4bf32e0a26adec1c89f
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-08-26 12:07:35 -0500

    added -amd64 suffix to amd64 executable

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-GOOS=linux GOARCH=amd64 go build -o ./runplugin ./cmd/runplugin 
+GOOS=linux GOARCH=amd64 go build -o ./runplugin-amd64 ./cmd/runplugin
 GOOS=linux GOARCH=arm64 go build -o ./runplugin-arm64 ./cmd/runplugin
