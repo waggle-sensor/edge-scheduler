460a6a7042dc059f196b52e56c55f8439cb34c52
Yongho Kim <ironyonghokim@gmail.com>
2022-01-28 09:51:51 -0600

    Updated cicd to push Docker image to Dockerhub

--- configure_nodescheduler.sh
+++ configure_nodescheduler.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-RMQ_NAME="wes-rabbitmq"
 nodeID=$1
 
 if [ -z ${nodeID} ]; then
@@ -8,33 +7,6 @@ if [ -z ${nodeID} ]; then
   echo "Usage: ./configure_nodescheduler.sh 00000000000001"
   exit 1
 fi
-
-rmqctl() {
-	# docker exec ${RMQ_NAME} rabbitmqctl "$@"
-	kubectl exec svc/${RMQ_NAME} -- rabbitmqctl "$@"
-}
-
-username="wes-plugin-scheduler"
-password="$(openssl rand -hex 20)"
-confperm=".*"
-writeperm=".*"
-readperm=".*"
-
-echo "Generating a RabbitMQ account for node scheduler..."
-# from waggle-edge-stack/kubernetes/update-rabbitmq-auth.sh
-# https://github.com/waggle-sensor/waggle-edge-stack/blob/main/kubernetes/update-rabbitmq-auth.sh
-(
-while ! rmqctl authenticate_user "$username" "$password"; do
-    while ! (rmqctl add_user "$username" "$password" || rmqctl change_password "$username" "$password"); do
-      sleep 3
-    done
-done
-
-while ! rmqctl set_permissions "$username" "$confperm" "$writeperm" "$readperm"; do
-  sleep 3
-done
-) &> /dev/null
-echo "Done"
 
 echo "Generating k3s objects for node scheduler..."
 cat <<EOF > nodescheduler.yaml
@@ -51,22 +23,15 @@ spec:
       labels:
         app: wes-plugin-scheduler
     spec:
+      priorityClassName: wes-high-priority
       serviceAccountName: wes-plugin-scheduler
       containers:
-      - image: waggle/scheduler:0.4.0
+      # TODO change to the real scheduler image :)
+      - image: waggle/scheduler:0.9.1
         name: wes-plugin-scheduler
         command: ["/app/nodescheduler"]
         args:
-        - "-nodeid"
-        - "${nodeID}"
-        - "-rabbitmq-uri"
-        - "${RMQ_NAME}:5672"
-        - "-rabbitmq-username"
-        - "${username}"
-        - "-rabbitmq-password"
-        - "${password}"
-        - "-rabbitmq-management-uri"
-        - "http://${RMQ_NAME}:15672"
+        - -in-cluster
 ---
 apiVersion: rbac.authorization.k8s.io/v1
 kind: RoleBinding
@@ -77,7 +42,7 @@ roleRef:
   kind: ClusterRole
   name: edit
   apiGroup: rbac.authorization.k8s.io
-  # 'edit' is a built-in cluster role. more info about these can be found here:
+  # `edit` is a built-in cluster role. more info about these can be found here:
   # https://kubernetes.io/docs/reference/access-authn-authz/rbac/#user-facing-roles
 subjects:
 - kind: ServiceAccount
@@ -88,6 +53,7 @@ apiVersion: v1
 kind: ServiceAccount
 metadata:
   name: wes-plugin-scheduler
+
 EOF
 
 echo "Done."
