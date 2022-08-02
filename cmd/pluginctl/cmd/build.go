ed0375624300db81f740a10d0010df93257b3e0c
Sean Shahkarami <sean.shahkarami@gmail.com>
2022-08-02 15:29:42 -0500

    added build command

---
+++ cmd/pluginctl/cmd/build.go
@@ -0,0 +1,71 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"os/signal"
+	"path/filepath"
+	"syscall"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	cmd := &cobra.Command{
+		Use:   "build PLUGIN_DIR",
+		Short: "Build plugin from directory",
+		Args:  cobra.ExactArgs(1),
+	}
+
+	flags := cmd.Flags()
+	tag := flags.String("t", "", "image tag")
+
+	/*
+		example
+		# cloned plugin-test-pipeline, cd'd in, made small code change
+		docker build -t 10.31.81.1:5000/seanshahkarami/plugin-test-pipeline .
+		docker push -10.31.81.1:5000/seanshahkarami/plugin-test-pipeline
+		pluginctl run -n plugin-test-pipeline 10.31.81.1:5000/seanshahkarami/plugin-test-pipeline
+	*/
+
+	cmd.RunE = func(cmd *cobra.Command, args []string) error {
+		path, err := filepath.Abs(args[0])
+		if err != nil {
+			return fmt.Errorf("invalid path: %s", err.Error())
+		}
+
+		name := filepath.Base(path)
+
+		if *tag == "" {
+			*tag = fmt.Sprintf("10.31.81.1:5000/waggle/%s", name)
+		}
+
+		ctx, _ := signal.NotifyContext(cmd.Context(), os.Interrupt, syscall.SIGTERM)
+
+		if err := runCommandContextStderr(ctx, "docker", "build", "-t", *tag, path); err != nil {
+			return err
+		}
+
+		if err := runCommandContextStderr(ctx, "docker", "push", *tag); err != nil {
+			return err
+		}
+
+		fmt.Fprintf(os.Stderr, "successfully build image!\n\n")
+
+		// print tag to stdout to make pipeable into other commands: pluginctl run -n my-plugin $(pluginctl build .)
+		fmt.Printf("%s\n", *tag)
+
+		return nil
+	}
+
+	rootCmd.AddCommand(cmd)
+}
+
+func runCommandContextStderr(ctx context.Context, name string, args ...string) error {
+	cmd := exec.CommandContext(ctx, name, args...)
+	cmd.Stdout = os.Stderr
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
