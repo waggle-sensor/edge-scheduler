0a0dd270bfeabbfa4a463be3463e384475eeac3d
Sean Shahkarami <sean.shahkarami@gmail.com>
2022-08-02 16:12:08 -0500

    dropped -t flag until we understand user needs. fixed typo and success message.

--- cmd/pluginctl/cmd/build.go
+++ cmd/pluginctl/cmd/build.go
@@ -13,42 +13,38 @@ import (
 )
 
 func init() {
+	const registry = "10.31.81.1:5000"
+	const namespace = "local"
+
 	cmd := &cobra.Command{
 		Use:   "build PLUGIN_DIR",
 		Short: "Build plugin from directory",
 		Args:  cobra.ExactArgs(1),
 	}
 
-	flags := cmd.Flags()
-	tag := flags.String("t", "", "image tag")
-
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
 		path, err := filepath.Abs(args[0])
 		if err != nil {
 			return fmt.Errorf("invalid path: %s", err.Error())
 		}
-
 		name := filepath.Base(path)
-
-		if *tag == "" {
-			*tag = fmt.Sprintf("10.31.81.1:5000/local/%s", name)
-		}
+		image := fmt.Sprintf("%s/%s/%s", registry, namespace, name)
 
 		// build and push to local registry. all output goes to stderr to allow easy piping
 		ctx, _ := signal.NotifyContext(cmd.Context(), os.Interrupt, syscall.SIGTERM)
 
-		if err := runCommandContextStderr(ctx, "docker", "build", "-t", *tag, path); err != nil {
+		if err := runCommandContextStderr(ctx, "docker", "build", "-t", image, path); err != nil {
 			return err
 		}
 
-		if err := runCommandContextStderr(ctx, "docker", "push", *tag); err != nil {
+		if err := runCommandContextStderr(ctx, "docker", "push", image); err != nil {
 			return err
 		}
 
-		fmt.Fprintf(os.Stderr, "successfully build image!\n\n")
+		fmt.Fprintf(os.Stderr, "Successfully built plugin\n\n")
 
 		// print tag to stdout to allow piping into other commands: pluginctl run -n my-plugin $(pluginctl build .)
-		fmt.Printf("%s\n", *tag)
+		fmt.Printf("%s\n", image)
 
 		return nil
 	}
