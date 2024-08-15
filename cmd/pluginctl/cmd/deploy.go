20e4c7519000b208a0cc9a908ca5f87027ba1f69
Yongho Kim <ironyonghokim@gmail.com>
2024-08-14 21:29:37 -0500

    Added a document for the pluginctl's resource option.

--- cmd/pluginctl/cmd/deploy.go
+++ cmd/pluginctl/cmd/deploy.go
@@ -22,7 +22,7 @@ func init() {
 	flags.StringVarP(&deployment.EnvFromFile, "env-from", "", "", "Set environment variables from file")
 	flags.BoolVar(&deployment.DevelopMode, "develop", false, "Enable the following development time features: access to wan network")
 	flags.StringVar(&deployment.Type, "type", "pod", "Type of the plugin. It is one of ['pod', 'job', 'deployment', 'daemonset]. Default is 'pod'.")
-	flags.StringVar(&deployment.ResourceString, "resource", "", "Specify resource requirement for running the plugin")
+	flags.StringVar(&deployment.ResourceString, "resource", "", "Specify resource requirement for running the plugin as a comma-separated list without spaces. For example, resource.cpu=1,limit.cpu=2.")
 	flags.BoolVar(&deployment.EnablePluginController, "enable-plugin-controller", false, "Enable plugin controller supporting the plugin")
 	flags.BoolVar(&deployment.ForceToUpdate, "force-to-update", false, "Force to create the plugin when failed to update it")
 	rootCmd.AddCommand(cmdDeploy)
